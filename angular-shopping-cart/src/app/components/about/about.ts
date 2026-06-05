f6334addae92d0752c8bec2bf337dcbc77c6fcf9
Yashasviid <yashasvidixitg.05@gmail.com>
2026-06-06 02:10:00 +0530

    feat: add About page component detailing values and founder

---
+++ angular-shopping-cart/src/app/components/about/about.ts
@@ -0,0 +1,16 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About implements OnInit {
+  private readonly titleService = inject(Title);
+
+  ngOnInit(): void {
+    this.titleService.setTitle('About Us - SkyCart');
+  }
+}
